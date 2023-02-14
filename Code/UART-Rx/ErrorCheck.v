53f99bbd8a58d3347933771ba186a7cfe0ff7ade
Mohamed Maged <majiid17@icloud.com>
2023-02-14 23:18:53 +0200

    Fixed

--- Code/UART-Rx/ErrorCheck.v
+++ Code/UART-Rx/ErrorCheck.v
@@ -17,10 +17,7 @@ module ErrorCheck(
     input wire  [1:0]  parity_type,   //  Parity type agreed upon by the Tx and Rx units.
     input wire  [7:0]  raw_data,      //  The 8-bits data separated from the data frame.
 
-    //  bus of three bits, each bit is a flag for an error
-    //  error_flag[0] ParityError flag, error_flag[1] StartError flag,
-    //  error_flag[2] StopError flag.
-    output reg [2:0]   error_flag
+    output wire [2:0]  error_flag     //  {stop_flag,start_flag,parity_flag}
 );
 
 //  Internal
@@ -29,13 +26,9 @@ reg parity_flag;
 reg start_flag;
 reg stop_flag;
 
-//  Encoding for the 4 types of the parity
+//  Encoding for types of the parity
 localparam ODD        = 2'b01,
-           EVEN       = 2'b10,
-           NOPARITY00 = 2'b00,
-           NOPARITY11 = 2'b11;
-
-//  Asynchronous Reset logic
+           EVEN       = 2'b10;
 
 //  Parity Check logic
 always @(*) 
@@ -49,24 +42,12 @@ end
 
 // Error Check logic
 always @(*) begin
-  if (reset_n && recieved_flag) 
-  begin
-    parity_flag  = (error_parity ^ parity_bit);
-    start_flag   = (start_bit || 1'b0);
-    stop_flag    = ~(stop_bit && 1'b1);
-  end
-  else
-  begin
-    parity_flag  = 1'b0;
-    start_flag   = 1'b0;
-    stop_flag    = 1'b0;
-  end
+  parity_flag  = (error_parity ^ parity_bit);
+  start_flag   = (start_bit || 1'b0);
+  stop_flag    = ~(stop_bit && 1'b1);
 end
 
 //  Output logic
-always @(*) 
-begin
-  error_flag = {stop_flag,start_flag,parity_flag};
-end
+assign error_flag = (reset_n && recieved_flag)? {stop_flag,start_flag,parity_flag} : 3'b0;
 
 endmodule
